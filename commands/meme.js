84105f4e8742c46fbb21def952fe9f3a5c400e82
jakanz <40873821+jakanz@users.noreply.github.com>
2021-07-02 23:11:38 -0600

    fixed bug, repeated var

--- commands/meme.js
+++ commands/meme.js
@@ -21,7 +21,7 @@ module.exports = {
                 message.channel.send({files: [`./media/M_vid/${vidChoose}`]})
                 break;
             default:
-                let imgChoose = memeImgFiles[Math.floor(Math.random() * memeImgFiles.length)]
+                let imgDefaultChoose = memeImgFiles[Math.floor(Math.random() * memeImgFiles.length)]
                 message.channel.send("You did not specify your preferred meme (or you simply misspelled the argument), so I defaulted to send an image. For further reference, the arguments for my meme command are `gif`, `img`, and `vid`.", {files: [`./media/M_img/${imgChoose}`]})
                 break;
         }
