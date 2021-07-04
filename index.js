bde1651df6ceb7d50b04d28978f093812430f72d
jakanz <40873821+jakanz@users.noreply.github.com>
2021-07-03 23:35:22 -0600

    updated index, added many json files for long arrays and config

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const Discord = require('discord.js');
-
 const fs = require('fs');
 const client = new Discord.Client();
-const commandPrefix = 'j!';
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const config = require('./json/config.json')
+const cmdRsrc = require('./json/cmdRsrc.json')
 
-client.login(process.env.BOT_TOKEN); // Retreive bot login token from Heroku config variables
+client.login(config.token) // Retreive bot login token from Heroku config variables
 client.on('ready', (async) => {
     console.log('k its online now do some shit yeah yeah'); // Confirm the bot's online
 
-    // Set random Discord status every 10000ms (10 seconds)
+    client.user.setActivity('BOOTING UP...')
     setInterval(() => {
-        const randomStatus = [JSON.parse(randomStatus)]
-        const status = randomStatus[Math.floor(Math.random() * randomStatus.length)]
-        client.user.setActivity(status);
-    },10000)
-})
+        const status = cmdRsrc.randomStatus[Math.floor(Math.random() * cmdRsrc.randomStatus.length)] // Select random status from status array
+        client.user.setActivity(status);},10000) // Set random Discord status every 10000ms (10 seconds)
+    }
+)
 
 client.commands = new Discord.Collection();
 for(const file of commandFiles){
@@ -24,13 +23,12 @@ for(const file of commandFiles){
 }
 
 client.on('message', message => {
-    if(!message.content.startsWith(commandPrefix) || message.author.bot) return; // If message does not contain the command prefix or if the message is written by the bot, ignore
-
-    const args = message.content.slice(commandPrefix.length).split(/ +/); // Slice off the space for bot to understand command (?)
+    if(!message.content.startsWith(config.prefix) || message.author.bot) return; // If message does not contain the command prefix or if the message is written by the bot, ignore
+    const args = message.content.slice(config.prefix.length).split(/ +/); // Slice off the space for bot to understand command (?)
     const command = args.shift().toLowerCase(); // If command has uppercase, shift whole command to lowercase
 
-    const commandList = ["help", "socials", "meme", "sex"]
-    if (Array.prototype.includes(command, commandList)) { // If a command from commandList is used (with prefix), execute command from file
+    const commandList = ["help", "meme", "punch", "socials"]
+    if (commandList.includes(command)) { // If a command from commandList is used (with prefix), execute command from file
         client.commands.get(command).execute(message,args);
         }
     }
